0f660ce1109d94c9f93062727eb15a65d9f3cde1
ranjeev3000 <73913671+ranjeev3000@users.noreply.github.com>
2020-11-30 00:07:43 +0530

    Add files via upload

---
+++ Python Assignment 2/Prob 1.py
@@ -0,0 +1,12 @@
+a ='*'
+i =0
+n =4
+
+for i in range(0,6):
+    print (a*i)
+
+
+for i in range(0,5):
+    print(a*(n-i))
+
+
---
+++ Python Assignment 2/Prob 2.py
@@ -0,0 +1,7 @@
+str = 'ineuron'
+
+b =len(str)
+
+
+c = str[b::-1]
+print(c)
